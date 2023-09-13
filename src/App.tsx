a40e084e8fe4d8c9fe45470b04a12af7b96dd28d
Luiz Alberto <lasilva@gmail.com>
2023-09-12 23:15:58 -0300

    Desenvolvimento da área de retorno de resultado para o usuário.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
   return (
@@ -19,6 +20,28 @@ export function App() {
           </Button>
         </div>
       </div>
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              placeholder="Inclua o prompt para a IA..."
+              className="resize-none p-5 leading-relaxed"
+            />
+            <Textarea
+              placeholder="Resultado gerado pela IA..."
+              className="resize-none p-5 leading-relaxed"
+              readOnly
+            />
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável{" "}
+            <code className="text-violet-400">{"{transcription}"}</code> no seu
+            prompnt para adicionar o conteúdo da transcrição do vídeo
+            selecionado.
+          </p>
+        </div>
+        <aside className="w-80"></aside>
+      </main>
     </div>
   );
 }
